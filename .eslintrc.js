6c6f6ad8f372f28252aef3695be2e773eca13d62
Darko Živić <zivic.darko79@gmail.com>
2023-12-09 16:18:53 +0100

    Modified an add new file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'react-hooks'],
   rules: {
     'no-console': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn', // <--- THIS IS THE NEW RULE
   },
   /* rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto'}],
